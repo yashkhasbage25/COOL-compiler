7dc26932d7dbbecfb70a7322368e8881693fcf96
yashkhasbage25 <yashkhasbage25@gmail.com>
2019-09-20 16:30:44 +0530

    pre-semantic check complete

--- A2/semantic/src/java/cool/Semantic.java
+++ A2/semantic/src/java/cool/Semantic.java
@@ -1,5 +1,7 @@
 package cool;
 
+import cool.AST.class_;
+
 public class Semantic{
 	private boolean errorFlag = false;
 	public void reportError(String filename, int lineNo, String error){
@@ -13,7 +15,233 @@ public class Semantic{
 /*
 	Don't change code above this line
 */
+
+	ScopeTable<AST.attr> scopeTable = new ScopeTable<AST.attr>();
+	ClassInfo classInfo = new ClassInfo();
+
 	public Semantic(AST.program program){
-		//Write Semantic analyzer code here
+
+		classInfo.fillDefaultClasses();
+		classInfo.createNewObjectInfo();
+		classInfo.createNewObjectInfo();
+
+		collectAndValidateClasses(program);
+		runCycleReporter(program);
+		checkForUndefinedParent(program);
+
+		analyzeClassFeatures(program);
+	}
+
+	private void collectAndValidateClasses(AST.program program) {
+
+		for(class_ programClass: program.classes) {
+			if (!isDefaultClassRedifined(programClass)) {
+				if (!classInheritanceNotPossible(prgramClass)) {
+					classInfo.classNameMapper.put(programClass.name, programClass);
+					classInfo.inheritanceGraph.addEdge(parentClassName, programClassName);
+				}
+			}
+		}
+	}
+
+	private boolean isDefaultClassRedifined(class_ programClass) {
+
+		boolean foundError = false;
+		String programClassName = programClass.name;
+		if (programClassName.equals(CoolUtils.OBJECT_TYPE_STR)) {
+			reportError(programClass.filename,
+				programClass.lineNo,
+				"Redefinition of Object class."
+			);
+			foundError = true;
+		} else if (programClassName.equals(CoolUtils.INT_TYPE_STR)) {
+			reportError(programClass.filename,
+				programClass.lineNo,
+				"Redefinition of Int class."
+			);
+			foundError = true;
+		} else if (programClassName.equals(CoolUtils.STRING_TYPE_STR)) {
+			reportError(programClass.filename,
+				programClass.lineNo,
+				"Redefinition of String class."
+			);
+			foundError = true;
+		} else if (programClassName.equals(CoolUtils.BOOL_TYPE_STR)) {
+			reportError(programClass.filename,
+				programClass.lineNo,
+				"Redefinition of Bool class."
+			);
+			foundError = true;
+		} else if (programClassName.equals(CoolUtils.IO_TYPE_STR)) {
+			reportError(programClass.filename,
+				programClass.lineNo,
+				"Redefinition of IO class."
+			);
+			foundError = true;
+		}
+		return foundError;
+	}
+
+	private boolean classInheritanceNotPossible(class_ programClass) {
+
+		boolean foundError = false;
+		String parentClassName = programClass.parent;
+		if (parentClassName.equals(CoolUtils.SELF_TYPE_STR)) {
+			reportError(programClass.filename,
+				programClass.lineNo,
+				"Parent class cannot be SELF_TYPE"
+			);
+			foundError = true;
+		} else if (parentClass.equals(CoolUtils.INT_TYPE_STR) ||
+				CoolUtils.equals(CoolUtils.STRING_TYPE_STR ||
+				CoolUtils.equals(CoolUtils.BOOL_TYPE_STR ))) {
+			reportError(programClass.filename,
+				programClass.lineNo,
+				"Class " + parentClassName + " can never be inherited. Attempt to inherit by class " + programClass.name
+			);
+			foundError = true;
+		}
+		return foundError;
+	}
+
+	private boolean checkForUndefinedParent(AST.program program) {
+		boolean foundError = false;
+		for (class_ programClass: program.classes) {
+			String parentClassName = programClass.parent;
+			if (classInfo.classNameMapper.get(parentClassName) == null) {
+				reportError(programClass.filename,
+					programClass.lineNo,
+					"Inheritance unsuccessful: " + programClass.name +
+					" could not inherit " + parentClassName + " because " +
+					parentClassName + " was not defined.");
+					foundError = true;
+			}
+		}
+		return foundError;
+	}
+
+	private boolean analyzeClassFeatures(AST.program program) {
+		Set<String> classesFoundSet = new HashSet<String>();
+		boolean foundMain = false;
+		boolean foundmain = false;
+		boolean mainHasArgs = false;
+		boolean foundError = false;
+
+		for (class_ programClass: program.classes) {
+			if (programClass.equals(CoolUtils.MAIN_TYPE_STR)) {
+				foundMain = true;
+			}
+
+			if (classesFoundSet.contains(programClass.name)) {
+				reportError(programClass.filename,
+					programClass.lineNo,
+					"Classes cannot be redefined. Class " + programClass.name +
+					" was attempted to be redefined."
+				);
+				foundError = true;
+			} else {
+				classesFoundSet.add(programClass.name);
+			}
+
+			Map<String, List<String>> classMethodName2Args = new HashMap<String, List<String>>();
+			Map<String, String> classAttrName2Type = new HashMap<String, String>();
+
+			for (AST.feature classFeature: programClass.features) {
+				if (classFeature instanceof AST.attr) {
+					AST.attr classAttr = (AST.attr) classFeature;
+					if (classAttrName2Type.contains(classAttr)) {
+						reportError(programClass.filename,
+							programClass.lineNo,
+							"Attribute " + classAttr.name.toString() + " is redefined."
+						);
+						foundError = true;
+					} else if (isAttrInherited(classAttr, programClass, classInfo)) {
+						reportError(programClass.filename,
+							programClass.lineNo,
+							"Attribute " + classAttr + " was inherited but still redefined."
+						);
+						foundError = true;
+					} else {
+						classAttrName2Type.put(classAttr.name, classAttr.typeid);
+					}
+				} else if (classFeature instanceof AST.method) {
+					AST.method classMethod = (AST.method) classFeature;
+					if (classAttrName2Type.containsKey(classMethod.name)) {
+						reportError(programClass.filename,
+							programClass.lineNo,
+							"Method " + classMethod.name + " is redefined."
+						);
+						foundError = true;
+					} else {
+						List<String> argTypeList = new ArrayList<String>();
+						Set<String> argFormalNames = new HashSet<String>();
+						for (AST.formal arg: m.formals) {
+							if (argFormalNames.contains(arg.name)) {
+								reportError(programClass.filename,
+									programClass.lineNo,
+									"Formal argument " + arg.name +
+									" was reused in definition of " +
+									classMethod.name + " in class " + programClass.name
+								);
+								foundError = true;
+							} else {
+								argFormalNames.add(arg.name);
+								if (arg.typeid.equals(CoolUtils.SELF_TYPE_STR)) {
+									reportError(programClass.filename,
+										programClass.lineNo,
+										"Formal parameter in definition of " +
+										classMethod.name + " of class " +
+										programClass.name + " cannot have type 'SELlineNoF_TYPE'"
+									);
+									foundError = true;
+								} else {
+									argTypeList.add(arg.typeid);
+								}
+							}
+						}
+					}
+
+					argTypeList.add(classMethod.typeid);
+					classMethodName2Args.put(classMethod.name, argTypeList);
+					if (programClass.name.equals(CoolUtils.MAIN_TYPE_STR)) {
+						foundMain = true;
+						if (classMethod.name.equals(CoolUtils.MAIN_FN_STR)) {
+							foundmain = true;
+							mainHasArgs = (argTypeList.size() == 1);
+						}
+					}
+				} else {
+					reportError(programClass.filename,
+						programClass.lineNo,
+						"Reached a forbidden line."
+					);
+					foundError = true;
+				}
+			}
+			classInfo.methodInfo.insert(className, classMethodName2Args);
+			classInfo.attrInfo.insert(className, classAttr2Name2Type);
+		}
+
+		analyzeMainClass(foundMain, foundmain, mainHasArgs, classInfo);
+	}
+
+	private boolean analyzeMainClass(boolean foundMain, boolean foundmain, boolean mainHasArgs, ClassInfo classInfo) {
+		boolean foundError = false;
+		if (!foundMain) {
+			reportError("", 0, "Class Main not found.");
+			foundError = true;
+		} else {
+			if (!foundmain) {
+				reportError("", 0, "main method not found in Class Main");
+				foundError = true;
+			} else {
+				if (!mainHasArgs) {
+					class_ classMain = classInfo.classNameMapper.get(CoolUtils.MAIN_TYPE_STR);
+					reportError(classMain.filename, classMain.lineNo, "In Class Main, 'main' method must not contain any arguments. Leave the arguments blank.");
+					foundError = true;
+				}
+			}
+		}
+		return foundError;
 	}
 }
